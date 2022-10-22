64425fc81f6da0957365a0cde34593d921ac8c85
afrociberdelio <contato@mikaelsouza.com.br>
2022-10-22 16:33:57 -0300

    Update server.py

--- server.py
+++ server.py
@@ -9,7 +9,6 @@ import os
 
 
 app = Flask(__name__)
-app.debug = True
 
 def write_json(data, filename='response.json'):
     with open(filename, 'w') as f:
@@ -75,4 +74,4 @@ def main():
     print(get_cmc_data('BTC'))
 
 if __name__ == '__main__':
-    app.run(port=8443)
+    app.run(debug=True, port=8443)
